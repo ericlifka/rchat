b1d52ecf178ebc3752c061e4ee9abd4f782da97d
Eric Lifka <eric.lifka@gmail.com>
2015-03-07 19:18:28 -0500

    attempt to check session, doesn't work because origin rules

--- app/src/app.js
+++ app/src/app.js
@@ -1,4 +1,12 @@
-React.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('app')
-);
+let baseUrl = `https://apps.ininsca.com`;
+let sessionUrl = `${baseUrl}/api/v2/session`;
+
+$.ajax({
+    url: sessionUrl
+})
+    .done(() => {
+        console.log('done', arguments);
+    })
+    .fail(() => {
+        console.log('fail', arguments);
+    });
