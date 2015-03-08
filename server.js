7fec2f7ad67b5b7dee335caaffb08672dfc23aca
Eric Lifka <eric.lifka@gmail.com>
2015-03-08 14:11:54 -0400

    trying to get web sockets forwarding, this isn't fun anymore

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var request = require('request');
+var http = require('http');
+var httpProxy = require('http-proxy');
+
 var apiUrl = "https://apps.ininsca.com";
 var realtimeUrl = apiUrl + "/realtime";
 var uiUrl = "http://localhost:4200";
 
 var app = express();
+//var proxy = httpProxy.createProxyServer({
+//    target: {
+//        host: realtimeUrl,
+//        port: 80
+//    },
+//    ws: true
+//});
+
 app.use('/directory-api', function (req, res) {
     var url = apiUrl + req.url;
     console.log('forwarding request to: ' + url);
@@ -12,7 +23,7 @@ app.use('/directory-api', function (req, res) {
 });
 app.use('/realtime', function (req, res) {
     var url = realtimeUrl + req.url;
-    console.log('forwarding realtime request to: ' + url);
+    console.log('\nforwarding:\n  ' + req.url + '\nto:\n  ' + url);
     req.pipe(request(url)).pipe(res);
 });
 app.use('/app', function (req, res) {
@@ -20,8 +31,13 @@ app.use('/app', function (req, res) {
     req.pipe(request(url)).pipe(res);
 });
 
-var server = app.listen(3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('Example app listening at http://%s:%s', host, port);
-});
+var server = http.createServer(app);
+//server.on('upgrade', function (req, socket, head) {
+//    proxy.ws(req, socket, head);
+//});
+
+server.listen(3000);
+
+var host = server.address().address;
+var port = server.address().port;
+console.log('Proxy server listenting at http://%s:%s', host, port);
