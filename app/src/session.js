a90e22e9517ffa27d77e4e8bc7a9a3344ea397af
Eric Lifka <eric.lifka@gmail.com>
2015-03-08 11:59:45 -0400

    pulling out a session class to manage authenticating

---
+++ app/src/session.js
@@ -0,0 +1,12 @@
+export default {
+    checkSession() {
+        return new Promise((resolve, reject) => {
+
+        });
+    },
+    login(username, password) {
+        return new Promise((resolve, reject) => {
+
+        });
+    }
+};
