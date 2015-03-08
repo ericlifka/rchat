c4459047ae9bd21ca08bb81e6b9f50942f25a22b
Eric Lifka <eric.lifka@gmail.com>
2015-03-08 13:28:37 -0400

    add some realtime code to test that realtime is working (its not)

--- app/src/app.js
+++ app/src/app.js
@@ -3,6 +3,41 @@ import Session from './session';
 
 function bootstrapApp() {
     console.log('bootstrap');
+
+    let rt = window.rt = new Realtime({
+        jidResource: 'emberchat',
+        jidRouting: true,
+        focusV2: true,
+        roomsV2: true,
+        offlineJoinNotifications: true,
+        debug: []
+    });
+
+    rt.on('connect', () => {
+        console.log('connect');
+    });
+
+    rt.on('disconnect', () => {
+        console.log('disconnect');
+    });
+
+    rt.on('message', () => {
+        console.log('message');
+    });
+
+    rt.on('status', () => {
+        console.log('status');
+    });
+
+    rt.on('activeChat', () => {
+        console.log('activeChat');
+    });
+
+    rt.on('presence', () => {
+        console.log('presence');
+    });
+
+    rt.connect();
 }
 
 Session.checkSession()
