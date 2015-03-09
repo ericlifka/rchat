353c2ad59df5d89659cc2e681e98607a150ac691
Eric Lifka <eric.lifka@gmail.com>
2015-03-08 23:29:55 -0400

    pull proxy handlers out

--- proxy.js
+++ proxy.js
@@ -5,6 +5,7 @@ var express = require('express');
 var httpProxy = require('http-proxy');
 
 var config = {
+    port: 3000,
     hosts: {
         directory: 'https://apps.ininsca.com',
         broccoli: 'http://localhost:4200'
@@ -19,45 +20,40 @@ var config = {
     }
 };
 
-var app = express();
 var proxy = httpProxy.createProxyServer(config.proxy);
-
-var proxyHttp = function (req, res) {
-    proxy.web(req, res, {
-        target: config.hosts.directory
-    });
+var proxies = {
+    directory: function (req, res) {
+        proxy.web(req, res, {
+            target: config.hosts.directory
+        });
+    },
+    files: function (req, res) {
+        proxy.web(req, res, {
+            target: config.hosts.broccoli
+        });
+    },
+    socket: function (req, socket, head) {
+        proxy.ws(req, socket, head, {
+            target: config.hosts.directory
+        });
+    }
 };
-
-var proxyUi = function (req, res) {
-    proxy.web(req, res, {
-        target: config.hosts.broccoli
-    });
-};
-
-var proxyWebsocket = function (req, socket, head) {
-    proxy.ws(req, socket, head, {
-        target: config.hosts.directory
-    });
-};
-
 proxy.on('error', function (e) {
     console.log('Proxy error: ', e);
 });
 
-app.all('/api/*', proxyHttp);
-app.all('/platform/api/*', proxyHttp);
-app.all('/directory/api/*', proxyHttp);
-app.all('/realtime/*', proxyHttp);
-app.all('/sessions/*', proxyHttp);
-app.all('/services/*', proxyHttp);
-app.all('/admin/*', proxyHttp);
-app.all('/*', proxyUi);
+var app = express();
+app.all('/api/*', proxies.directory);
+app.all('/platform/api/*', proxies.directory);
+app.all('/directory/api/*', proxies.directory);
+app.all('/realtime/*', proxies.directory);
+app.all('/sessions/*', proxies.directory);
+app.all('/services/*', proxies.directory);
+app.all('/admin/*', proxies.directory);
+app.all('/*', proxies.files);
 
 var server = https.createServer(config.https, app);
+server.on('upgrade', proxies.socket);
+server.listen(config.port);
 
-server.on('upgrade', proxyWebsocket);
-
-var port = 3000;
-server.listen(port);
-
-console.log('Serving SSL from:', port);
+console.log('Serving SSL from: ' + config.port);
