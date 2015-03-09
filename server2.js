e2c7dc127f37b7644d23d6ddc69b5ef0b4a725d0
Eric Lifka <eric.lifka@gmail.com>
2015-03-08 22:42:05 -0400

    add proxy for ui files

--- server2.js
+++ server2.js
@@ -15,6 +15,8 @@ var defaultTarget = {
     carrierPigeon: 'wss://carrier-pigeon.us-east-1.ininsca.com'
 };
 
+var uiHost = "http://localhost:4200";
+
 var app = express();
 
 var proxyTarget;
@@ -33,6 +35,12 @@ var proxyHttp = function (req, res) {
 
     proxy.web(req, res, {
         target: target
+    });
+};
+
+var proxyUi = function (req, res) {
+    proxy.web(req, res, {
+        target: uiHost
     });
 };
 
@@ -56,11 +64,8 @@ proxy.on('error', function (e) {
     console.log('Error while trying to proxy. Check internet connection.');
 });
 
-app.use(static({ urlRoot: '/dca', directory: 'dist' }));
-app.use(static({ urlRoot: '/tca', directory: 'dist' }));
-app.use(static({ urlRoot: '/sca', directory: 'dist' }));
-app.use(static({ urlRoot: '/prod', directory: 'dist' }));
 
+api.all('/ui/*', proxyUi);
 app.all('/api/*', proxyHttp);
 app.all('/platform/api/*', proxyHttp);
 app.all('/directory/api/*', proxyHttp);
@@ -77,49 +82,3 @@ var port = 3000;
 server.listen(port);
 
 console.log('Serving SSL from:', port);
-
-function static(options) {
-    return function (req, res, next) { // Gotta catch 'em all (and serve index.html)
-        var filePath = "";
-        if (options.directory) {
-            var regex = new RegExp('^' + (options.urlRoot || ''));
-            // URL must begin with urlRoot's value
-            if (!req.path.match(regex)) {
-                next();
-                return;
-            }
-            filePath = options.directory + req.path.replace(regex, '');
-        } else if (options.file) {
-            filePath = options.file;
-        } else {
-            throw new Error('static() isn\'t properly configured!');
-        }
-
-        fs.stat(filePath, function (err, stats) {
-            if (err) {
-                next();
-                return;
-            } // Not a file, not a folder => can't handle it
-
-            if (options.ignoredFileExtensions) {
-                if (options.ignoredFileExtensions.test(req.path)) {
-                    res.status(404).send({error: 'Resource not found'});
-                    return; // Do not serve index.html
-                }
-            }
-
-            // Is it a directory? If so, search for an index.html in it.
-            if (stats.isDirectory()) {
-                filePath = path.join(filePath, 'index.html');
-            }
-
-            // Serve the file
-            res.sendFile(path.resolve(filePath), function (err) {
-                if (err) {
-                    next();
-                    return;
-                }
-            });
-        });
-    };
-}
