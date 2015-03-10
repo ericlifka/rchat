d3dcdf0ca4c2003fe54cabce9612cbfa9e755b22
Eric Lifka <eric.lifka@gmail.com>
2015-03-09 20:01:45 -0400

    create a class to house the application and store state

--- app/src/app.js
+++ app/src/app.js
@@ -1,51 +1,57 @@
 import Credentials from './credentials';
 import Session from './session';
+import realtime from './realtime';
+
+class ChatApp {
+    constructor() {
+        this.realtime = realtime();
+        this.attachToEvents();
+    }
+
+    connect() {
+        this.realtime.connect();
+    }
+
+    attachToEvents() {
+
+    }
+}
+
+const app = new ChatApp();
 
 function bootstrapApp() {
-    console.log('bootstrap');
-
-    let rt = window.rt = new Realtime({
-        jidResource: 'emberchat',
-        jidRouting: true,
-        focusV2: true,
-        roomsV2: true,
-        offlineJoinNotifications: true,
-        debug: []
-    });
-
-    rt.on('connect', () => {
-        console.log('connect');
-    });
-
-    rt.on('disconnect', () => {
-        console.log('disconnect');
-    });
-
-    rt.on('message', () => {
-        console.log('message');
-    });
-
-    rt.on('status', () => {
-        console.log('status');
-    });
-
-    rt.on('activeChat', () => {
-        console.log('activeChat');
-    });
-
-    rt.on('presence', () => {
-        console.log('presence');
-    });
-
-    rt.connect();
+    app.connect();
 }
 
 Session.checkSession()
     .then(bootstrapApp)
-    .catch(() => {
-        Session.login(Credentials.username, Credentials.password)
-            .then(bootstrapApp)
-            .catch(() => {
-                console.log("Error - couldn't log in");
-            });
-    });
+    .catch(() => Session.login(Credentials.username, Credentials.password)
+        .then(bootstrapApp)
+        .catch(() => console.log("Error - couldn't log in")));
+
+//
+//rt.on('connect', () => {
+//    console.log('connect');
+//});
+//
+//rt.on('disconnect', () => {
+//    console.log('disconnect');
+//});
+//
+//rt.on('message', () => {
+//    console.log('message');
+//});
+//
+//rt.on('status', () => {
+//    console.log('status');
+//});
+//
+//rt.on('activeChat', () => {
+//    console.log('activeChat');
+//});
+//
+//rt.on('presence', () => {
+//    console.log('presence');
+//});
+//
+//rt.connect();
